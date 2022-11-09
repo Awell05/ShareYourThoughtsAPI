301dcf189187ea28e9ec499dddc1632198619fb1
Novo Persona <adcaldwell93@yahoo.com>
2022-11-09 00:41:11 -0500

    finished User.js model

--- models/User.js
+++ models/User.js
@@ -13,9 +13,25 @@ const userSchema = new mongoose.Schema
         unique: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
     thoughts: {
-
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
     },
-    friends: {
-        
-    }
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
 });
+
+userSchema.virtual('friendCount').get( function () {
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+module.exports = userSchema
